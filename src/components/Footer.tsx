e0b69cb0d5d86ab3a0d4d0c2fcfbff578a5794bd
SaraAzmin <azmin.sara17@gmail.com>
2023-08-16 00:05:36 +0200

    added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
+import { ExternalLink } from "react-external-link";
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  var currentYear = new Date().getFullYear();
+  return (
+    <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-orange-600 md:flex items-center justify-between">
+      <Link href="/" className="font-bold text-xl">
+        Pizzaria
+      </Link>
+      <p>
+        © {currentYear}&nbsp;
+        <ExternalLink
+          className="font-semibold"
+          href="https://www.linkedin.com/in/azminsara17/"
+          target="_blank"
+        >
+          Sara Azmin
+        </ExternalLink>{" "}
+        &nbsp;| All rights Reserved
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
