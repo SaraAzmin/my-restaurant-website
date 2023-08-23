dd8cdffe1f27268c2e7e5b92a850a722c7cf3507
SaraAzmin <azmin.sara17@gmail.com>
2023-08-23 23:51:42 +0200

    added featured items section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Banner from '@/components/Banner'
+import FeaturedProduct from '@/components/FeaturedProduct'
 
 export default function Home() {
   return (
     <main>
       <Banner></Banner>
+      <FeaturedProduct></FeaturedProduct>
     </main>
   )
 }
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -21,14 +21,15 @@ const data = [
 ];
 
 const Banner = () => {
-
   return (
     <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-orange-50">
       <div className="flex-1 flex items-center justify-center flex-col gap-5 text-orange-600 font-bold">
         <h1 className="text-4xl text-center uppercase p-2 md:p-10 md:text-5xl xl:text-6xl">
-        the best pizza to share with your loved ones!
+          the best pizza to share with your loved ones!
         </h1>
-        <button className="bg-orange-600 text-white px-8 py-4 rounded-lg hover:border-orange-600 hover:bg-white hover:text-orange-600 border-2 border-transparent">Order Now</button>
+        <button className="bg-orange-600 text-white px-8 py-4 rounded-lg hover:border-orange-600 hover:bg-white hover:text-orange-600 border-2 border-transparent">
+          Order Now
+        </button>
       </div>
       <div className="w-full flex-1 relative">
         <Image
